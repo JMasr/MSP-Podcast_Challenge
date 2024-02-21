f2a198c2607974ce47d720f09a17e93e13d6ee7b
JMasr <moscaelectronica@gmail.com>
2024-02-21 02:30:08 +0100

    Adding wav2vec base-960

--- utils/etc.py
+++ utils/etc.py
@@ -3,13 +3,17 @@ import torch
 import time
 import numpy as np
 import json
+
+
 def set_deterministic(seed):
     torch.manual_seed(seed)
     np.random.seed(seed)
 
+
 def get_ssl_type(ssl_type):
-    ssl_book={
+    ssl_book = {
         "wav2vec2-large-robust": "facebook/wav2vec2-large-robust",
+        "wav2vec2-base-960": "facebook/wav2vec2-base-960h",
         "wavlm-large": "microsoft/wavlm-large"
     }
     return ssl_book.get(ssl_type, None)
