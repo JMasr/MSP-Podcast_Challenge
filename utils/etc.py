dd905d3dad861f972513fb88da5edc8fc5cba803
JMasr <moscaelectronica@gmail.com>
2024-02-22 10:47:08 +0100

    Adding more Wav2Vec models

--- utils/etc.py
+++ utils/etc.py
@@ -12,7 +12,9 @@ def set_deterministic(seed):
 
 def get_ssl_type(ssl_type):
     ssl_book = {
-        "wav2vec2-large-robust": "facebook/wav2vec2-large-robust",
+        "wav2vec2-bert": "facebook/w2v-bert-2.0",
+        "wav2vec2-xl": "facebook/wav2vec2-large-960h-lv60-self",
+        "wav2vec2-l-robust": "facebook/wav2vec2-large-robust",
         "wav2vec2-base-960": "facebook/wav2vec2-base-960h",
         "wavlm-large": "microsoft/wavlm-large"
     }
